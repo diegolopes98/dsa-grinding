0e6fd100219497e6e504b5b17e1172d1465ad53b
Diego Pereira Lopes <diegopereiralopes98@gmail.com>
2025-03-22 17:30:37 -0300

    feat: add BinarySearch with generic implementation

---
+++ leetcode/src/main/java/com/diegolopes98/dsa/leetcode/Array/BinarySearch/BinarySearch.java
@@ -0,0 +1,5 @@
+package com.diegolopes98.dsa.leetcode.Array.BinarySearch;
+
+public interface BinarySearch<T extends Comparable<T>> {
+    int search(T[] items, T target);
+}
---
+++ leetcode/src/main/java/com/diegolopes98/dsa/leetcode/Array/BinarySearch/BinarySearchImpl.java
@@ -0,0 +1,23 @@
+package com.diegolopes98.dsa.leetcode.Array.BinarySearch;
+
+public class BinarySearchImpl<T extends Comparable<T>> implements BinarySearch<T> {
+    @Override
+    public int search(T[] items, T target) {
+        int left = 0, right = items.length - 1;
+
+        while (left <= right) {
+            final int mid = left + (right - left) / 2;
+            final T curr = items[mid];
+            final int compare = curr.compareTo(target);
+
+            if (compare == 0 ) {
+                return mid;
+            }
+
+            if (compare > 0) right = mid - 1;
+            else left = mid + 1;
+        }
+
+        return -1;
+    }
+}
