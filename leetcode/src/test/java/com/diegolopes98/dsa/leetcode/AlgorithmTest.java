00194d5167065854b4ea30008614c709cc29c7b8
Diego Pereira Lopes <diegopereiralopes98@gmail.com>
2025-03-24 00:55:06 -0300

    refactor(neetcode): static method convention for methods to be implemented

--- leetcode/src/test/java/com/diegolopes98/dsa/leetcode/AlgorithmTest.java
+++ leetcode/src/test/java/com/diegolopes98/dsa/leetcode/AlgorithmTest.java
@@ -2,9 +2,16 @@ package com.diegolopes98.dsa.leetcode;
 
 import com.diegolopes98.dsa.ds.AlgorithmImplementation;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.stream.Stream;
+
 public abstract class AlgorithmTest<IN, OUT> {
+    static Stream<Arguments> provideArguments() {
+        throw new UnsupportedOperationException("Must be implemented");
+    }
+
     @ParameterizedTest(name = "[{index}] given {1}")
     @MethodSource("provideArguments")
     void givenParametrizedArguments_whenCallingImplementation_shouldRunAssertions(
