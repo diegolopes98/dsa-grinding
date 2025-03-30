1ee6029b7119f029b01cad575da07e7f4ba6c10f
Diego Pereira Lopes <diegopereiralopes98@gmail.com>
2025-03-30 15:31:25 -0300

    test(leetcode): update LongestCommonPrefix to use new test base class

--- leetcode/src/test/java/com/diegolopes98/dsa/leetcode/String/LongestCommonPrefix/LongestCommonPrefixTest.java
+++ leetcode/src/test/java/com/diegolopes98/dsa/leetcode/String/LongestCommonPrefix/LongestCommonPrefixTest.java
@@ -1,45 +1,29 @@
 package com.diegolopes98.dsa.leetcode.String.LongestCommonPrefix;
 
-import com.diegolopes98.dsa.leetcode.AlgorithmTest;
+import com.diegolopes98.dsa.ds.AlgorithmImplementation;
+import com.diegolopes98.dsa.leetcode.ParameterizedAlgorithmTest;
 import com.diegolopes98.dsa.leetcode.TestAssertion;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.provider.Arguments;
 
 import java.util.stream.Stream;
 
-class LongestCommonPrefixTest extends AlgorithmTest<LongestCommonPrefixInput, String> {
-    static Stream<Arguments> provideArguments() {
-        final var implementation = new LongestCommonPrefixTraverseImpl();
+class LongestCommonPrefixTest extends ParameterizedAlgorithmTest<LongestCommonPrefixInput, String> {
+
+    @Override
+    protected Stream<Arguments> provideArguments() {
         return Stream.of(
                 Arguments.of(
-                        implementation,
                         LongestCommonPrefixInput.with(new String[]{"foo", "foobar", "foc"}),
                         TestAssertion.expectedOutputConsumer("fo", Assertions::assertEquals)
-                ), Arguments.of(
-                        implementation,
-                        LongestCommonPrefixInput.with(new String[]{"flower", "flow", "flight"}),
-                        TestAssertion.expectedOutputConsumer("fl", Assertions::assertEquals)
-                ), Arguments.of(
-                        implementation,
-                        LongestCommonPrefixInput.with(new String[]{"flower", "flow", "fein"}),
-                        TestAssertion.expectedOutputConsumer("f", Assertions::assertEquals)
-                ), Arguments.of(
-                        implementation,
-                        LongestCommonPrefixInput.with(new String[]{"foo", "banana", "foc"}),
-                        TestAssertion.expectedOutputConsumer("", Assertions::assertEquals)
-                ), Arguments.of(
-                        implementation,
-                        LongestCommonPrefixInput.with(new String[]{"", "f"}),
-                        TestAssertion.expectedOutputConsumer("", Assertions::assertEquals)
-                ), Arguments.of(
-                        implementation,
-                        LongestCommonPrefixInput.with(new String[]{""}),
-                        TestAssertion.expectedOutputConsumer("", Assertions::assertEquals)
-                ), Arguments.of(
-                        implementation,
-                        LongestCommonPrefixInput.with(new String[]{}),
-                        TestAssertion.expectedOutputConsumer("", Assertions::assertEquals)
                 )
+        );
+    }
+
+    @Override
+    protected Stream<AlgorithmImplementation<LongestCommonPrefixInput, String>> provideImplementations() {
+        return Stream.of(
+                new LongestCommonPrefixTraverseImpl()
         );
     }
 }
